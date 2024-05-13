d8eb103172f7291b978a18ebf57e53301f7dee00
Gabriel <gabrielfilipe2346@gmail.com>
2024-05-13 13:54:37 -0300

    subindo cardapio

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    fontFamily:{
+      'sans': ['Poppins', 'sans-serif']
+    },
+    extend: {
+      backgroundImage:{
+        "home": "url('/Assets/bg.png')"
+    }},
+  },
+  plugins: [],
+}
+
